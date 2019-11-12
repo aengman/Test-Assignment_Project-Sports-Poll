38a8ac461e36ad425d8f83332837db93a50a0a55
Albin Engman <albin.a.engman@gmail.com>
2019-11-12 22:59:53 +0100

    Updates

---
+++ precache-manifest.b796a8afb4e98600ec22827988477fa4.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891755dc86ab46f651891e39139b0d9d",
+    "url": "/Test-Assignment---Project-Sports-Poll/index.html"
+  },
+  {
+    "revision": "7e194a0051081e7266a9",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/css/main.5bd06d5a.chunk.css"
+  },
+  {
+    "revision": "1cc0e8491ecdce972a8c",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/js/2.8f25382e.chunk.js"
+  },
+  {
+    "revision": "7e194a0051081e7266a9",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/js/main.259b0641.chunk.js"
+  },
+  {
+    "revision": "0c7a0743fdbc42a0130d",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/js/runtime-main.25ee23e5.js"
+  },
+  {
+    "revision": "2d42b314581ee8db4fca322df4cb4e6f",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/footballSilhouette.2d42b314.png"
+  },
+  {
+    "revision": "1f8bb438477872c67d1f8327a4deb571",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/handballSilhouette.1f8bb438.png"
+  },
+  {
+    "revision": "0a26f2de6adfcd20a8037545f53ae305",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/icehockeySilhouette.0a26f2de.png"
+  },
+  {
+    "revision": "a62bd1c623d985e88201af76f8bc0486",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/snookerSilhouette.a62bd1c6.jpg"
+  },
+  {
+    "revision": "bdf86a5234ca4cbcb086a352381da517",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/sportsArena.bdf86a52.jpg"
+  },
+  {
+    "revision": "33e3ce737ee273d2cbceaec175a7f419",
+    "url": "/Test-Assignment---Project-Sports-Poll/static/media/tennisSilhouette.33e3ce73.png"
+  }
+]);
